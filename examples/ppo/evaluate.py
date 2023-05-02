3732120de06bf6a99ae64a42fa01499daeaad6d5
Sadhana Lolla <lolla.sadhana@gmail.com>
2023-05-02 11:31:11 -0400

    removing moving average norm for now

--- examples/ppo/evaluate.py
+++ examples/ppo/evaluate.py
@@ -22,10 +22,12 @@ def evaluate(
     eval_envs = make_vec_envs(env_name, robot_structure, seed + num_processes, num_processes,
                               None, eval_log_dir, device, True)
 
+    '''
     vec_norm = utils.get_vec_normalize(eval_envs)
     if vec_norm is not None:
         vec_norm.eval()
         vec_norm.obs_rms = obs_rms
+    '''
 
     eval_episode_rewards = []
 
