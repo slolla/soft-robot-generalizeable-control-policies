b8b9062be92ae517b6a489efea93ccd139078013
Sadhana Lolla <lolla.sadhana@gmail.com>
2023-05-03 00:09:10 -0400

    model and training utilities

---
+++ examples/generalizeable_control/model.py
@@ -0,0 +1,68 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os, sys
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.join(curr_dir, '..')
+sys.path.insert(1, root_dir)
+sys.path.insert(1, os.path.join(root_dir, 'externals', 'pytorch_a2c_ppo_acktr_gail'))
+
+from a2c_ppo_acktr.model import NNBase
+
+class CNN(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+        # input is 2x6x6
+        self.conv1 = nn.Conv2d(input_dim, 25, kernel_size=3, padding=1) 
+        self.conv2 = nn.Conv2d(25, 50, kernel_size=3, padding=1) 
+        self.conv3 = nn.Conv2d(50, 50, kernel_size=3, padding=1) 
+        self.conv4 = nn.Conv2d(50, 50, kernel_size=3, padding=1) 
+        #self.conv5 = nn.Conv2d(50, 100, kernel_size=3, padding=1)
+        #self.conv6 = nn.Conv2d(100, 100, kernel_size=3, padding=1)
+        #self.conv7 = nn.Conv2d(100, 100, kernel_size=3, padding=1)  
+        #self.conv8 = nn.Conv2d(100, 50, kernel_size=3, padding=1) 
+        self.conv9 = nn.Conv2d(50, 1, kernel_size=3, padding=1) 
+        self.batchnorm1 = nn.BatchNorm2d(50)
+        #self.batchnorm2 = nn.BatchNorm2d(100)
+        self.batchnorm3 = nn.BatchNorm2d(1)
+        
+        self.flatten = nn.Flatten()
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x)) # SMALL
+        x = self.batchnorm1(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x)) # MEDIUM
+        #x = F.relu(self.conv5(x))
+        #x = F.relu(self.conv6(x))
+        #x = F.relu(self.conv7(x))
+        #x = self.batchnorm2(x)
+        #x = F.relu(self.conv8(x))
+        x = 0.5*torch.tanh(self.batchnorm3(self.conv9(x))) + .1
+        x = self.flatten(x)
+        return x
+
+class UniversalController(NNBase):
+    def __init__(self, num_inputs, recurrent=False, hidden_size=36):
+        super(UniversalController, self).__init__(recurrent, num_inputs, hidden_size)
+        if recurrent:
+            num_inputs = hidden_size
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
+                               constant_(x, 0), np.sqrt(2))
+
+        self.actor = CNN(num_inputs)
+
+        self.train()
+
+    def forward(self, inputs, rnn_hxs=None, masks=None):
+        x = inputs
+
+        if self.is_recurrent:
+            x, rnn_hxs = self._forward_gru(x, rnn_hxs, masks)
+
+        hidden_actor = self.actor(x)
+
+        return hidden_actor, rnn_hxs
---
+++ examples/generalizeable_control/train.py
@@ -0,0 +1,122 @@
+import glob
+import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import TensorDataset, DataLoader
+from model import UniversalController
+
+class MaskedMSELoss(torch.nn.Module):
+    def __init__(self):
+        super(MaskedMSELoss, self).__init__()
+    
+    def forward(self, output, target, mask):
+        loss = (torch.flatten(output) - torch.flatten(target)) ** 2.0 * torch.flatten(mask)
+        loss = torch.sum(loss)/torch.sum(mask)
+        return loss
+
+class GeneralizeableControllerTrainer:
+    def __init__(self, data_path, generations, within_robot=False, sequential=True):
+        self.data_path = data_path
+        self.within_robot = within_robot
+        self.sequential=sequential
+        self.generations = generations
+        self.actor_loss = MaskedMSELoss()
+        self.model = UniversalController(13)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-5)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.model = self.model.to(self.device)
+        print("USING", self.device)
+    
+    def create_across_robot_datasets(self):
+        if self.sequential:
+            train_gens = np.arange(self.generations)
+            test_gens = [self.generations]
+        else:
+            test_gens = [np.random.choice(self.generations)]
+            train_gens = [i for i in np.arange(self.generations) if i not in test_gens]
+        
+        train_dataset = self.get_dataset_from_gens(train_gens)
+        test_dataset = self.get_dataset_from_gens(test_gens)
+        return train_dataset, test_dataset
+        
+    def get_dataset_from_gens(self, gens):
+        all_states = []
+        all_actions = []
+        all_masks = []
+
+        for gen in gens:
+            path = os.path.join(self.data_path, "generation_{}/rollouts/*.npz".format(gen))
+            filenames = glob.glob(path)
+            for f in filenames:
+                rollout = np.load(f)
+                all_states.extend(rollout["state"])
+                all_actions.extend(rollout["action"])
+                all_masks.extend(rollout["state"][:, 3, :, :,] + rollout["state"][:, 4, :, :])
+
+        return TensorDataset(torch.FloatTensor(all_states), torch.FloatTensor(all_actions), torch.FloatTensor(all_masks))
+
+    def _train(self, dataloader, epoch):
+        self.model.train()
+        epoch_loss = 0.0
+        running_loss = 0.0
+        for i, data in enumerate(dataloader):
+            states, actions, masks = data
+            states, actions, masks = states.to(self.device), actions.to(self.device), masks.to(self.device)
+            self.optimizer.zero_grad()
+
+            output_actions, _ = self.model(states)
+            
+            actor_loss = self.actor_loss(output_actions, actions, masks)
+            loss = actor_loss
+            loss.backward()
+
+            self.optimizer.step()
+
+            running_loss += loss.item()
+            epoch_loss += loss.item()
+            if i % 1000 == 999:
+                last_loss = running_loss / 1000 # loss per batch
+                print('  batch {} loss: {}'.format(i + 1, last_loss))
+                running_loss = 0.
+        print("dividing by", i)
+        print('epoch', epoch, 'loss', epoch_loss/i)
+        return epoch_loss/(i * 16)
+        
+    
+    def _test(self, dataloader, epoch):
+        self.model.eval()
+        running_loss = 0.0
+        for i, data in enumerate(dataloader):
+            states, actions, masks = data
+            states, actions, masks = states.to(self.device), actions.to(self.device), masks.to(self.device)
+            
+            with torch.no_grad():
+
+                output_actions, _ = self.model(states)
+                actor_loss = self.actor_loss(output_actions, actions, masks)
+                loss = actor_loss
+
+            running_loss += loss.item()
+        print("dividing by", i)
+        print("testing epoch", epoch, "loss: ", running_loss/i)
+        return running_loss/(i * 16)
+        
+    def collect_and_train(self):
+        train_rollout_dataset, test_rollout_dataset = self.create_across_robot_datasets()
+        
+        train_rollout_dataloader = DataLoader(train_rollout_dataset, shuffle=True, batch_size=16)
+        test_rollout_dataloader = DataLoader(test_rollout_dataset, shuffle=True, batch_size=16)
+
+        train_losses = []
+        test_losses = []
+        num_epochs = 50
+        for epoch in range(num_epochs):
+            train_losses.append(self._train(train_rollout_dataloader, epoch))
+            test_losses.append(self._test(test_rollout_dataloader, epoch))
+        plt.plot(np.arange(len(train_losses)), train_losses, label="train")
+        plt.plot(np.arange(len(train_losses)), test_losses, label="test")
+        plt.savefig("/home/sadhana/soft-robot-generalizeable-control-policies/examples/generalizeable_control/test.png")
+
+trainer = GeneralizeableControllerTrainer("/home/sadhana/soft-robot-generalizeable-control-policies/data/walker-no-normalization", 10)
+trainer.collect_and_train()
